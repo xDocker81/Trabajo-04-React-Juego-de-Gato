b7a9564a0978fd84ca4840b2ddff8f7f8206ee38
Alex <yair.ramirez2960@alumnos.udg.mx>
2022-05-20 14:50:32 -0500

    Updates

---
+++ precache-manifest.340d74ede8d306e692919552201ebfb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e4f60853b17b63cbbbad4457d9133b",
+    "url": "/Trabajo-04-React-Juego-de-Gato/index.html"
+  },
+  {
+    "revision": "f24ba10f8594bae68e2c",
+    "url": "/Trabajo-04-React-Juego-de-Gato/static/css/main.336ec073.chunk.css"
+  },
+  {
+    "revision": "c5fcff42bf7081fbff75",
+    "url": "/Trabajo-04-React-Juego-de-Gato/static/js/2.61e06272.chunk.js"
+  },
+  {
+    "revision": "f24ba10f8594bae68e2c",
+    "url": "/Trabajo-04-React-Juego-de-Gato/static/js/main.a42b806e.chunk.js"
+  },
+  {
+    "revision": "3976c9b5226a5682bb4a",
+    "url": "/Trabajo-04-React-Juego-de-Gato/static/js/runtime~main.dfadf883.js"
+  }
+]);
